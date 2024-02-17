7ed419fbff42323ff10c8e3451a41a7b2f3f5025
Kmilasc <kamillaandrade77@gmail.com>
2024-02-17 01:31:16 -0300

    feature(adding register page)

--- src/pages/register/RegisterPage.tsx
+++ src/pages/register/RegisterPage.tsx
@@ -1,3 +1,61 @@
-export function RegisterPage() {
-    return <></>
+import { useForm } from 'react-hook-form';
+import './styles.css';
+import { useNavigate } from 'react-router-dom';
+import { useRegisterMutation } from '../../services/slices/authApi';
+import { useEffect } from 'react';
+
+interface NewUser {
+  username: string;
+  password: string;
+  email: string;
 }
+
+// TODO: Add loader
+// TODO: Make it pretty
+// TODO: Do a responsive design
+export function Register() {
+  const { register, handleSubmit, formState: { errors, isValid } } = useForm<NewUser>();
+  const navigate = useNavigate()
+  const [registerMutation, { isLoading, isSuccess }] = useRegisterMutation();
+
+  const onSubmit = (data: NewUser) => {
+    registerMutation(data).unwrap();
+  };
+
+  useEffect(() => {
+    isSuccess && navigate('/login')
+  }, [isSuccess, navigate])
+
+  return (
+    <div className="register-container">
+      <h2>Register</h2>
+      <form onSubmit={handleSubmit(onSubmit)}>
+        <div className="form-group">
+          <label>Username:</label>
+          <input
+            type="text"
+            {...register("username", { required: 'Username é obrigatório' })}
+          />
+          {errors.username && <p>{errors.username.message}</p>}
+        </div>
+        <div className="form-group">
+          <label>Email:</label>
+          <input
+            type="email"
+            {...register("email", { required: 'Email é obrigatório' })}
+          />
+          {errors.email && <p>{errors.email.message}</p>}
+        </div>
+        <div className="form-group">
+          <label>Password:</label>
+          <input
+            type="password"
+            {...register("password", { required: 'Senha é obrigatória' })}
+          />
+          {errors.password && <p>{errors.password.message}</p>}
+        </div>
+        <button type="submit" disabled={isLoading || !isValid}>Register</button>
+      </form>
+    </div>
+  );
+};
