973a7f8311b408057806f5b145517509d8dd4d6a
Kmilasc <kamillaandrade77@gmail.com>
2024-02-17 01:05:02 -0300

    feature(creating slice to login and register)

--- src/services/store/store.ts
+++ src/services/store/store.ts
@@ -1,15 +1,17 @@
 import { configureStore } from "@reduxjs/toolkit";
 import { bookApi } from "../slices/booksApiSlice";
 import booksSlice from "../slices/booksSlice";
+import { authApi } from "../slices/authApi";
 
 export const store = configureStore({
-    reducer: {
-        books: booksSlice,
-        [bookApi.reducerPath]: bookApi.reducer
-    },
-    middleware: (getDefaultMiddleware) => {
-        return getDefaultMiddleware().concat(bookApi.middleware)
-    }
+  reducer: {
+    books: booksSlice,
+    [bookApi.reducerPath]: bookApi.reducer,
+    [authApi.reducerPath]: authApi.reducer,
+  },
+  middleware: (getDefaultMiddleware) => {
+    return getDefaultMiddleware().concat(bookApi.middleware, authApi.middleware)
+  }
 });
 
 export type RootState = ReturnType<typeof store.getState>;
